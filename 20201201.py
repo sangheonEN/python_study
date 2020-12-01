380364b2e9b94da971ba5e82bf952de001a6cc4b
sangheonEN <jteks6@gmail.com>
2020-12-01 20:13:59 +0900

    20201201

---
+++ 20201201.py
@@ -0,0 +1,106 @@
+# a = 10
+# b = 20
+# print(a+b)
+#
+# INPUT 함수
+# c = input("데이터를 넣어 주세요.") # scanf()
+# print(c)
+
+# A = input("A : ")
+# B = input("B : ")
+# # print는 기본적으로 개행이 된다 하지만, 마지막에 , end=''을 붙이면 ''빈공간이 만나면 만나면 끝나게됨
+# print("A+B = ", end='')
+# print(int(A) + int(B))
+
+# CASE 1
+# 전역변수 : a 지역변수 : b,c
+# a = 10
+# print("데이터를 입력하시오.", end="")
+# d = input()
+# def func(b):
+#     c = a + b
+#     return c
+# print(func(int(d)))
+
+# CASE 2
+# a = 10
+# print('데이터를 입력하시오.')
+# def A(b, c):
+#     d = a+b+c
+#     return d
+# def Square():
+#     b = a*a
+#     return b
+# C = 10* Square()
+# print(A(20, 30))
+# print(C)
+
+# def go():
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+# print(go(), end='')
+# print(go(), end='')
+# print(go(), end='')
+
+
+# ADD 사용자 정의 함수 만들기 OR 함수 안에서 PRINT() 하기
+# a = int(input())
+# b = int(input())
+#
+# def add(a, b):
+#     c = a + b
+#     return c
+# def minus(a, b):
+#     c = a - b
+#     return c
+#
+# print(add(a, b))
+# print(minus(a, b))
+
+# 단순 조건문
+# a = int(input())
+# def A(a):
+#     b = 20
+#     c = a+b
+#     return c
+# if A(a)>=40:
+#     print(A(a))
+# else:
+#     print("총합이 40이 넘지 않았습니다.")
+
+# 문자열 연산 및 개요
+# print("안녕 \"클레오\" 파트라~ 세상에서 \t제일 가\n는 포테\n이토 칩!")
+# a = "이년아"
+# b = "뭐 썅련아"
+# print((a + b+"\t") * 5)
+
+# 문자열 인덱싱 및 슬라이스 기법
+# a = "Hello World"
+# print((a[0]+a[3]))
+# print(a[-1])
+# print(a[2:3])
+# print(a[2:4])
+# print(a[2:5])
+# print(a[2:6])
+# print(a[2:7])
+# print(a[:-2])
+# print(a[0:-2])
+# print(a[1:-2])
+# print(a[:])
+# print(a[0:7:2]) # 0~7인덱스 까지 2씩 올라가면서 출력 a[0], a[2], a[4], a[6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string.py
@@ -0,0 +1,41 @@
+# 문자열 연산 및 개요
+# print("안녕 \"클레오\" 파트라~ 세상에서 \t제일 가\n는 포테\n이토 칩!")
+# a = "이년아"
+# b = "뭐 썅련아"
+# print((a + b+"\t") * 5)
+
+# 문자열 인덱싱 및 슬라이스 기법
+# a = "Hello World"
+# print((a[0]+a[3]))
+# print(a[-1])
+# print(a[2:3])
+# print(a[2:4])
+# print(a[2:5])
+# print(a[2:6])
+# print(a[2:7])
+# print(a[:-2])
+# print(a[0:-2])
+# print(a[1:-2])
+# print(a[:])
+# print(a[0:7:2]) # 0~7인덱스 까지 2씩 올라가면서 출력 a[0], a[2], a[4], a[6]
+
+# # 문자열 변환 replace(기존 문자열, 변환할 문자열) 다른 변수에 넣어 준 뒤에 사용할 수 있음
+# a = "HELLO WORLD DSAF"
+# c = "20"
+# # a.replace('HELLO','HIGH')
+# # print(a)
+# b = a.replace("HELLO", "HIGH")
+# print(b)
+# print(a.count('L'))
+# print(a.find('WORLD')) #해당 문자열이 맞지 않으면 -1 반환
+# print(a.upper()) # 대문자 변환
+# print(a.lower()) # 소문자 변환
+# print(a.strip("HELLO")) # 해당 문자열 빼고 나머지 문자열 출력
+# print(a.split(" ")) # 어떤 기준으로 문자열을 잘라서 각각의 문자열 배열로 만듬.
+# print(a.zfill(50)) # 변수로 넣는 정수 만큼 자리수를 만듬. 문자열이 자리수 보다 적으면 앞에 0을 채워 넣음.
+# print(c.zfill(5))
+# d = int(c)
+# print(d+2902)
+
+
+
