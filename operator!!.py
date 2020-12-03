4eb31df116f6333863b8dcc7fcfc68ed6664ec27
sangheonEN <jteks6@gmail.com>
2020-12-03 20:17:16 +0900

    2020_12_3
    2020_12_3

---
+++ operator!!.py
@@ -0,0 +1,34 @@
+# 증감 연산자          c/c++은 ++, -- 있지만 파이썬은 없음
+# a = 1
+# a += 10
+# print("a += 10 : ", a)
+# a -= 10
+# print("a -= 10 : ", a)
+# a *= 10
+# print("a *= 10 : ", a)
+# a /= 10
+# print("a /= 10 : ", a)
+# a //= 10
+# print("a //= 10 : ", a)
+# a %= 10
+# print("a %= 10 : ", a)
+
+# 관계 연산자
+# 문자열 또한 즉시 비교가능!!!
+# a = "abc"
+# b = "def"
+# print(a == b)
+# print(a < b)      # 기본적으로 문자열 비교 사전 순으로 비교하기 때문에! 즉, a가 d보다 작은지 큰지 판별
+
+# 논리 연산자 : and, or, not
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+# print(not a)
+# print(not b)
+
+# if 3 > 5 or 7 < 10:
+#     print("야호")
+#
+# print(3 > 5 or 7 < 10)
