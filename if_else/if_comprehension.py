5592b573d0fc40fa210cf1a91939919727185bc7
Sang Heon LEE <jteks6@gmail.com>
2024-10-14 18:51:08 +0900

    if 문법 공부
    if 문
    - A if B else C
    - if not False

---
+++ if_else/if_comprehension.py
@@ -0,0 +1,34 @@
+# def A():
+#     return True
+
+
+# def B():
+#     return False
+
+
+# def C():
+#     return False
+
+
+if __name__ == "__main__":
+
+    """
+        B가 True(참)일 경우 A가 반환됩니다.
+        B가 False(거짓)일 경우 C가 반환됩니다.
+    """
+    A = True
+    BT = True
+    BF = False
+    C = False
+
+    print(A if BT else C)
+    print(A if BF else C)
+
+
+    """
+    if not False:
+        print("참입니다.")
+    """
+    
+    if not False:
+        print("참입니다.")
